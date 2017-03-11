951e3b3280c52ccf2dac6b11afcf519e26888028
Kenya Shields <kenyashields@Kenyas-MacBook-Pro.local>
2017-03-11 07:39:54 -0600

    exercise-4 answers

---
+++ exercise-4.js
@@ -0,0 +1,41 @@
+var sum1 = 8+15;
+console.log (sum1);
+
+var sum2 = 8+15+(-100);
+console.log (sum2);
+
+var sum3 = (-5)+32.8;
+console.log (sum3);
+
+var sum4 = 35-2.8;
+console.log (sum4);
+
+var sum5 = 35-2.8-17;
+console.log (sum5);
+
+var sum6 = (-5)+(32-8);
+console.log (sum6);
+
+var sum7 = 75+(-35)-2.8;
+console.log (sum7);
+
+var sum8 = 6*(-0.1);
+console.log (sum8);
+
+var sum9 = -8*5*1000000000.8;
+console.log (sum9);
+
+var sum10 = 5/-12;
+console.log (sum10);
+
+var sum11 = 25/-12/2;
+console.log (sum11);
+
+var sum12 = 25%4;
+console.log (sum12);
+
+var sum13 = (5+(-4))-((-4*3)/2.3)%9
+console.log (sum13);
+
+
+
